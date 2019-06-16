417b19bff6013b7bc4c63b26ba080ae0deeefb8f
Bert Pablo <bert.w.pablo@gmail.com>
2019-06-15 23:07:40 -0400

    created upload page

--- brite_obs/urls.py
+++ brite_obs/urls.py
@@ -17,11 +17,12 @@ urlpatterns = [
 
     url(r'^search/$', search_views.search, name='search'),
 
+    url(r'^obs_submission/$', obs_submission.as_view()),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     url(r'', include(wagtail_urls)),
-    url(r'^obs_submission/', obs_submission.as_view())
+
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    url(r'^pages/', include(wagtail_urls)),
--- observations/views.py
+++ observations/views.py
@@ -19,6 +19,25 @@ class obs_submission(TemplateView):
 
         return render(request, self.template_name, {'form' : form})
     
+    def post(self, request):
+        print("POST request", request.POST)
+        print("FILE request", request.FILES)
+ #       obsfile = request.FILES['obsfile']
+        form = ObsForm(request.POST, request.FILES)
+        print("THIS IS A DAMN Form", form)
+        obsfile = form.cleaned_data['obsfile']
+        field = form.cleaned_data['field']
+#   #      print("request files", request.FILES)
+#   #      print(obsfile.name)
+#   #      obsfile.read().decode('UTF-8')
+#         print("this is a field", field)
+#         print("this is a file", obsfile)
+        data = obsfile.read().decode('UTF-8')
+
+        return render(request, self.template_name)
+
+    
+            
     # obsfile = request.FILES['file']
 
     # if not obsfile.name.endswith('.csv'):
